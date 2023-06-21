61dd9219fbf044003a29bb414fb6fd75d9435adb
chun <1316953144@qq.com>
2023-06-21 10:22:31 +0800

    添加项目文件。

---
+++ Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Console.WriteLine("请输入一个大于一的整数");
+            string y = Console.ReadLine();
+            for (int i = 2; i <= int.Parse(y); i++)
+            {
+                bool z = true;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        z = false;
+                        break;
+                    }
+                }
+
+                if (z)
+                {
+                    x++;
+                }
+
+            }
+            Console.WriteLine("其中有{0}个质数", x);
+        }
+    }
+}
